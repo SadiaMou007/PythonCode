0a4f525a37b5a7b91037c0867e4972aaff082743
Your Name <you@example.com>
2021-10-27 08:48:00 -0700

    if else

---
+++ FormattedString.py
@@ -0,0 +1,6 @@
+variable1 = int(input("First number: "))
+variable2 = int(input("Second number: "))
+
+# print formatted string
+
+print(f"{variable1} + {variable2} = {variable1 + variable2}")
--- typeconversion.py
+++ typeconversion.py
@@ -2,12 +2,15 @@
 #without typecast--
 v1 = input("Enter num1: ")
 v2 = input("Enter num2: ")
-sum=v1+v2
+sum = v1+v2
 print("Sum is :"+sum)
 
 #With typecast
-v1 = int(input("Enter num1: "))
-v2 = int(input("Enter num2: "))
-sum=v1+v2
-print("Sum is :" ,sum),
+
+v3 = int(input("Enter num1: "))
+v4 = int(input("Enter num2: "))
+sum = v3+v4
+
+print("Sum is :" ,sum,)
+
 
